444a537247b16a9b9728a793987a03acdb0c60ba
Vaibhav Ahuja <19bcs1065@cuchd.in>
2021-11-30 23:52:08 +0530

    data entry

---
+++ Movie Recommendation System/main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+from flask import Flask, render_template, request
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+import bs4 as bs
+import urllib.request
+import pickle
+import requests
+def create_similarity():
+    data = pd.read_csv('main_data.csv')
+    # creating a count matrix
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(data['comb'])
+    # creating a similarity score matrix
+    similarity = cosine_similarity(count_matrix)
+    return data,similarity
+a = 45
